aa4cc4717fda4eb7d43c0b1685284c7924c5cb50
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2022-05-31 15:21:16 +0300

    Add policy and terms

---
+++ src/common/footer/PrivacyPolicy.tsx
@@ -0,0 +1,13 @@
+const PrivacyPolicy = () => {
+
+  return (
+    <div>
+      <iframe
+        src="https://docs.google.com/document/d/e/2PACX-1vQPEQb7iQKpxPHB4Az8MrusWpdhCwh7sgtsMfPuN8GKxGxYWm3gGl7KRcxVdf0fAVGP2Fu8eJVHNU1q/pub?embedded=true"
+        style={{ width: "800px", height: "50vh" }}
+      ></iframe>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
---
+++ src/common/footer/TermsOfService.tsx
@@ -0,0 +1,11 @@
+const TermsOfService = () => {
+
+  return (
+    <iframe
+      src="https://docs.google.com/document/d/e/2PACX-1vRsph8tijOXYUyPKid1bN6WegEczX6KECeeLfptEI8IFcESiNiZycoFuQkH2j4dDX2IseB7Et27Ofn4/pub?embedded=true"
+      style={{ width: "800px", height: "50vh" }}
+    ></iframe>
+  );
+};
+
+export default TermsOfService;
